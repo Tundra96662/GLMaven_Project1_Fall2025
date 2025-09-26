3e33fee9ce9f83c12760889d04f658449a3549a7
Tundra <73200419+Tundra96662@users.noreply.github.com>
2025-09-26 12:26:23 -0500

    Update GenericQueue.java

--- GLMaven_Project1_Fall2025/src/main/java/GenericQueue.java
+++ GLMaven_Project1_Fall2025/src/main/java/GenericQueue.java
@@ -1,15 +1,75 @@
 public class GenericQueue<T> extends GenericList<T>{
-	Node<T> tail;
+	private Node<T> tail;
 
 	public GenericQueue(T newData){
-		//IMPLEMENT ME PLEASE
+		//First node - y
+        Node<T> firstNode = new Node<>();
+        firstNode.data = newData;
+        firstNode.next = null;
+
+        //head and tail point to first node
+        setHead(firstNode);
+        tail = firstNode;
+        setLength(1);
+
 	}
+
+    @Override
 	public void add(T data){
+	    Node<T> newNode = new Node<>();
+        newNode.data = data;
+        newNode.next = null;
 
+        if (getHead() == null) {
+            setHead(newNode);
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        setLength(getLength() + 1);
 	}
 
+    public void add(T data, int code) {
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+        newNode.code = code;
+        newNode.next = null;
+
+        if (getHead() == null) {
+            setHead(newNode);
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        setLength(getLength() + 1);
+
+    }
+
+    @Overide
 	public T delete(){
+        if (getHead() == null) {
+            return null;
+        }
+        //Remove current head node
+        T deletedData = getHead.data;
+        setHead(getHead().next);
+        setLength(getLength() -1);
 
-		return null; //Fix me if you get the chance.
+        if (getHead() == null) {
+            tail = null;
+        }
+		return deletedData;
 	}
+
+
+	public void enqueue(T data) {
+            add(data);
+	}
+
+    public T dequeue(){
+       return delete();
+    }
+
 }
