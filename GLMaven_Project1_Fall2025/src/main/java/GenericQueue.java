4d65cce328cce930f18cf7f280c9be26b41c5a12
Tundra <73200419+Tundra96662@users.noreply.github.com>
2025-09-27 13:19:12 -0500

    Updated

--- GLMaven_Project1_Fall2025/src/main/java/GenericQueue.java
+++ GLMaven_Project1_Fall2025/src/main/java/GenericQueue.java
@@ -1,159 +1,77 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 
-public class MyHashMap <T> implements Iterable<T>{
-    private ArrayList<GenericQueue<T>> map;
-    private int size;
+public class GenericQueue<T> extends GenericList<T>{
+	private Node<T> tail;
 
-    public MyHashMap(String key, T value){
-        map = new ArrayList<>(10);
-        //Arraylist with 10 items being null
-        for (int i = 0; i < 10; i++) {
-            map.add(null);
+	public GenericQueue(T newData){
+		//First node - y
+        Node<T> firstNode = new Node<>();
+        firstNode.data = newData;
+        firstNode.next = null;
+
+        //head and tail point to first node
+        setHead(firstNode);
+        tail = firstNode;
+        setLength(1);
+
+	}
+
+    @Override
+	public void add(T data){
+	    Node<T> newNode = new Node<>();
+        newNode.data = data;
+        newNode.next = null;
+
+        if (getHead() == null) {
+            setHead(newNode);
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
         }
-        put(key, value);
-    }
+        setLength(getLength() + 1);
+	}
 
-    public void put(String key, T value){
-        int hashCode = key.hashCode();
-        int hashValue = hashCode & 9;
+    public void add(T data, int code) {
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+        newNode.code = code;
+        newNode.next = null;
 
-        GenericQueue<T> queue = map.get(hashValue);
-
-        if (queue == null) {
-            queue = new GenericQueue<>(value);
-            //First node
-            queue.getHead().code = hashCode;
-            map.set(hashValue, queue);
-            size++;
-        } else {//Exists
-            queue.add(value, hashCode);
-            size++;
+        if (getHead() == null) {
+            setHead(newNode);
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
         }
+        setLength(getLength() + 1);
 
-    }
-
-    //If key in hash map
-    public boolean contains(String key) {
-        int hashCode = key.hashCode();
-        int hashValue = hashCode & 9;
-
-        GenericQueue<T> queue = map.get(hashValue);
-        if (queue == null) {
-            return false;
-        }
-
-        GenericList<T>.Node<T> curr = queue.getHead();
-        while (curr != null) {
-            if (curr.code == hashCode) {
-                return true;
-            }
-            curr = curr.next;
-        }
-        return false;
-    }
-
-    //Get value by key
-    public T get(String key) {
-        int  hashCode = key.hashCode();
-        int hashValue = hashCode & 9;
-
-        GenericQueue<T> queue = map.get(hashValue);
-        if (queue == null) {
-            return null;
-        }
-
-        //Finding matching hash code
-        GenericList<T>.Node<T> curr = queue.getHead();
-        while (curr != null) {
-            if (curr.code == hashCode) {
-                return  curr.data;
-            }
-            curr = curr.next;
-        }
-        return null;
-    }
-
-
-    public int size() {
-        return size;
-    }
-
-    //is map empty
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    //Replace a val for a key
-    public T replace(String key, T value){
-        int hashCode = key.hashCode();
-        int hashValue = hashCode & 9;
-
-        GenericQueue<T> queue = map.get(hashValue);
-        if (queue == null) {
-            return null;
-        }
-        //Look for matching hash code
-        GenericList<T>.Node<T> curr = queue.getHead();
-        while (curr != null) {
-            if (curr.code == hashCode) {
-                T oldValue = curr.data;
-                curr.data = value;
-
-                return oldValue;
-            }
-            curr = curr.next;
-        }
-        return null;
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private int currBucket = 0;
-            private GenericList<T>.Node<T> currNode = null;
-            {
-                findNextNode();
-            }
-            private void findNextNode() {
-                if (currNode != null) {
-                    currNode = currNode.next;
-                }
-                while (currNode == null && currBucket < 10) {
-                    if (map.get(currBucket) != null) {
-                        currNode = map.get(currBucket).getHead();
-                    }
-                    currBucket++;
-                }
-            }
-            @Override
-            public boolean hasNext() {
-                return currNode != null;
-            }
+	public T delete(){
+        if (getHead() == null) {
+            return null;
+        }
+        //Remove current head node
+        T deletedData = getHead().data;
+        setHead(getHead().next);
+        setLength(getLength() -1);
 
-            @Override
-            public T next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                T data = currNode.data;
-                findNextNode();
-                return data;
-            }
-        };
+        if (getHead() == null) {
+            tail = null;
+        }
+		return deletedData;
+	}
+
+
+	public void enqueue(T data) {
+            add(data);
+	}
+
+    public T dequeue(){
+       return delete();
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
