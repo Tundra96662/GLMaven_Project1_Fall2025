cf53b1c6013a66317f105b4d09ebacfe6d276ee2
Tundra <73200419+Tundra96662@users.noreply.github.com>
2025-09-27 22:14:29 -0500

    Added description on HashMap

--- GLMaven_Project1_Fall2025/src/main/java/GLProject.java
+++ GLMaven_Project1_Fall2025/src/main/java/GLProject.java
@@ -1,9 +1,15 @@
 /*
 	Daniel Loera	dloer2	dloer2@uic.edu
+	Yonatan De La Mora ydela4 ydela4@uic.edu
 
 
-	Description:
-
+	Description of HashMap: It implements a key-value storage system by using hashing.
+	It also includes precautions to handle collisions by chaining the new key-value pairs to those
+	already found in the bucket. Each bucket is made up of a linked list; that's how collision
+	was avoided, and the linked list works as a queue. Keys in the same bucket are differentiated
+	from one another by implementing codes. It is built using an 'iterator' which helps us
+	iterate through all the values in the HashMap.
+	
 
 */
 
