01418050104ff7ad3ee89508c0c01eb52b658d02
Bullsquidz <daniel.loera.060@gmail.com>
2025-09-27 20:17:15 -0500

    I dont know how but implements iterable wasnt there anymore. Fixed

--- GLMaven_Project1_Fall2025/src/main/java/GenericList.java
+++ GLMaven_Project1_Fall2025/src/main/java/GenericList.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-
-public abstract class GenericList<T> {
+public abstract class GenericList<T> implements Iterable<T>{
 	
 	private Node<T> head;
 	private int length;
