9fa6ead10f106457b115984b13266869cafc81f0
Tundra <73200419+Tundra96662@users.noreply.github.com>
2025-09-28 19:50:35 -0500

    Update MyHashMap.java

--- GLMaven_Project1_Fall2025/src/main/java/MyHashMap.java
+++ GLMaven_Project1_Fall2025/src/main/java/MyHashMap.java
@@ -3,11 +3,11 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 
-public class MyHashMap <T> implements Iterable<T>{
+public class MyHashMap <T> implements Iterable<T> {
     private ArrayList<GenericQueue<T>> map;
     private int size;
 
-    public MyHashMap(String key, T value){
+    public MyHashMap(String key, T value) {
         map = new ArrayList<>(10);
         //Arraylist with 10 items being null
         for (int i = 0; i < 10; i++) {
@@ -16,7 +16,7 @@ public class MyHashMap <T> implements Iterable<T>{
         put(key, value);
     }
 
-    public void put(String key, T value){
+    public void put(String key, T value) {
         int hashCode = key.hashCode();
         int hashValue = hashCode & 9;
 
@@ -57,7 +57,7 @@ public class MyHashMap <T> implements Iterable<T>{
 
     //Get value by key
     public T get(String key) {
-        int  hashCode = key.hashCode();
+        int hashCode = key.hashCode();
         int hashValue = hashCode & 9;
 
         GenericQueue<T> queue = map.get(hashValue);
@@ -69,7 +69,7 @@ public class MyHashMap <T> implements Iterable<T>{
         GenericList<T>.Node<T> curr = queue.getHead();
         while (curr != null) {
             if (curr.code == hashCode) {
-                return  curr.data;
+                return curr.data;
             }
             curr = curr.next;
         }
@@ -87,7 +87,7 @@ public class MyHashMap <T> implements Iterable<T>{
     }
 
     //Replace a val for a key
-    public T replace(String key, T value){
+    public T replace(String key, T value) {
         int hashCode = key.hashCode();
         int hashValue = hashCode & 9;
 
@@ -111,49 +111,7 @@ public class MyHashMap <T> implements Iterable<T>{
 
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private int currBucket = 0;
-            private GenericList<T>.Node<T> currNode = null;
-            {
-                findNextNode();
-            }
-            private void findNextNode() {
-                if (currNode != null) {
-                    currNode = currNode.next;
-                }
-                while (currNode == null && currBucket < 10) {
-                    if (map.get(currBucket) != null) {
-                        currNode = map.get(currBucket).getHead();
-                    }
-                    currBucket++;
-                }
-            }
-            @Override
-            public boolean hasNext() {
-                return currNode != null;
-            }
+        return new HMIterator<T>(map);
 
-            @Override
-            public T next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                T data = currNode.data;
-                findNextNode();
-                return data;
-            }
-        };
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
